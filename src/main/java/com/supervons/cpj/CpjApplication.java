86402b5e08988ce8263d8b5251f62edabef9108b
supervons <supervons@sina.com>
2019-03-07 09:06:59 +0800

    1，mvn jar包方式打包项目。 mvn clean 后 mvn install 导出的jar包到对应服务器 java -jar xxx.jar 运行即可。
    1, mvn jar package method package project.
    After mvn clean mvn install the exported jar package to the corresponding server java -jar xxx.jar to run.

--- src/main/java/com/supervons/cpj/CpjApplication.java
+++ src/main/java/com/supervons/cpj/CpjApplication.java
@@ -4,14 +4,21 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-public class CpjApplication {
+public class CpjApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CpjApplication.class, args);
     }
 
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
 
